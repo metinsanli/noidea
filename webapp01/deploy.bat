47b4040e4c1b035fcdd86b6f3d874b2d50417619
metinsanli <metinsanli@gmail.com>
2017-12-28 14:58:16 +0300

    maven, jsf, tomcat, primefaces örnek güncellendi.

--- webapp01/deploy.bat
+++ webapp01/deploy.bat
@@ -1,4 +1,8 @@
 ::DEPLOY TO APP SERVER
-mvn clean tomcat7:redeploy
+::mvn clean tomcat7:redeploy
 
-::OPEN TO WEB BROWSER = http://[ip-address]:8080/webapp01
+::RUN LOCAL TOMCAT SERVER
+mvn clean tomcat7:run-war
+
+::OPEN TO WEB BROWSER
+::http://[ip-address]:8080/webapp01
